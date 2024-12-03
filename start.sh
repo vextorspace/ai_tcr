d516a3d56c1afd5cebab76775252b72af287178d
Douglas Ronne <vextorspace@gmail.com>
2024-12-03 12:18:53 -0800

    Add .env and /target to .gitignore Create Cargo.toml for ai_tcr_string_calc Add ai_coder binary to bin directory Add commit_message binary to bin directory Create mod.rs in src/calc Add test.rs with basic test in src/calc Create lib.rs with calc module Add main.rs with Hello, world! Add start.sh script for watchexec Add tcr.sh script for test and commit

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+watchexec -i build -i .idea -i .git -i .gradle -i .kotlin -i .vscode -- ./tcr.sh
---
+++ tcr.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "Running gradle tests..."
+cargo test --test
+cargo test calc
+
+# Store the test result
+TEST_RESULT=$?
+
+# Check if tests failed
+if [ $TEST_RESULT -ne 0 ]; then
+    echo "Tests failed! Removing changes..."
+    git reset --hard HEAD
+else
+    echo "Tests passed! Committing..."
+    git add .
+    COMMIT_MESSAGE=`commit_message`
+    git commit -m "$COMMIT_MESSAGE"
+fi
+
+exit 0
