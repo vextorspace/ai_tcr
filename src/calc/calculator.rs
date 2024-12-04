fd621070271b3e6907798d0f34f3595472e65846
Douglas Ronne <vextorspace@gmail.com>
2024-12-04 15:26:59 -0800

    Add evaluate method to Calculator struct

--- src/calc/calculator.rs
+++ src/calc/calculator.rs
@@ -4,6 +4,10 @@ impl Calculator {
     pub fn new() -> Self {
         Calculator
     }
+
+    pub fn evaluate(&self, _expression: &str) -> String {
+        "result".to_string()
+    }
 }
 
 #[cfg(test)]
@@ -13,5 +17,12 @@ mod tests {
     #[test]
     fn instantiate() {
         let _ = Calculator::new();
+    }
+
+    #[test]
+    fn evaluate_returns_string() {
+        let calc = Calculator::new();
+        let result: String = calc.evaluate("2+3");
+        assert!(!result.is_empty())
     }
 }
