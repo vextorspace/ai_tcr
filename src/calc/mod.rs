a6c2e51d74af3903c0665358832ca0bb957527eb
Douglas Ronne <vextorspace@gmail.com>
2024-12-04 15:34:16 -0800

    cleaned out mod.rs

--- src/calc/mod.rs
+++ src/calc/mod.rs
@@ -1,20 +1,2 @@
 pub mod calculator;
-pub mod parser {
-    pub struct Parser;
-
-    impl Parser {
-        pub fn new() -> Self {
-            Parser
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::parser::Parser;
-
-    #[test]
-    fn test_parser_creation() {
-        let _ = Parser::new();
-    }
-}
+pub mod parser;
