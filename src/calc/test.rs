3643035f8ab2e6c4a11ad19c7cf8636f6c7cc33d
Douglas Ronne <vextorspace@gmail.com>
2024-12-04 10:26:05 -0800

    Add unit tests for Test struct methods

--- src/calc/test.rs
+++ src/calc/test.rs
@@ -9,3 +9,19 @@ impl Test {
         a + b
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn good_test() {
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn add_test() {
+        let result = Test::new().add(1, 2);
+        assert_eq!(result, 3);
+    }
+}
