2c514c8744ceb87013c671dcc0c99c3804c6ee4b
Douglas Ronne <vextorspace@gmail.com>
2024-12-04 12:30:58 -0800

    Rename failing_test to addition test

--- tests/acceptance_tests.rs
+++ tests/acceptance_tests.rs
@@ -3,7 +3,7 @@ mod tests {
     use ai_tcr_string_calc::calc::calculator::Calculator;
 
     #[test]
-    fn failing_test() {
+    fn addition() {
         let calculator = Calculator::new();
         let result = calculator.evaluate("2 + 3");
         assert_eq!(result, 5);
