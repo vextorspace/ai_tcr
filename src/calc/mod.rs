1546a07af865080dace93bcca9b35fe5bcdcb085
Douglas Ronne <vextorspace@gmail.com>
2024-12-04 15:33:37 -0800

    Add parser module with basic struct and test

--- src/calc/mod.rs
+++ src/calc/mod.rs
@@ -1,2 +1,20 @@
 pub mod calculator;
+pub mod parser {
+    pub struct Parser;
 
+    impl Parser {
+        pub fn new() -> Self {
+            Parser
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parser::Parser;
+
+    #[test]
+    fn test_parser_creation() {
+        let _ = Parser::new();
+    }
+}
