19bc44c28ae04f1b2a2acd0075cd8689857b35d3
Douglas Ronne <vextorspace@gmail.com>
2024-12-04 12:18:41 -0800

    Add Calculator struct with evaluate method

--- tests/acceptance_tests.rs
+++ tests/acceptance_tests.rs
@@ -1,4 +1,25 @@
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+struct Calculator;
+
+impl Calculator {
+    fn new() -> Self {
+        Calculator
+    }
+
+    fn evaluate(&self, expression: &str) -> i32 {
+        let parts: Vec<&str> = expression.split_whitespace().collect();
+        let a: i32 = parts[0].parse().unwrap();
+        let b: i32 = parts[2].parse().unwrap();
+        add(a, b)
+    }
+}
+
 #[test]
 fn failing_test() {
-   assert_eq!(1,2);
+    let calculator = Calculator::new();
+
+    calculator.evaluate("2 + 3");
 }
