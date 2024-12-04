06a6b8753ee9b83dae84b725c1832df11bd1ffa3
Douglas Ronne <vextorspace@gmail.com>
2024-12-04 12:31:38 -0800

    Add subtraction support to Calculator.

--- src/calc/calculator.rs
+++ src/calc/calculator.rs
@@ -9,7 +9,11 @@ impl Calculator {
         let parts: Vec<&str> = expression.split_whitespace().collect();
         let left: i32 = parts[0].parse().unwrap();
         let right: i32 = parts[2].parse().unwrap();
-        left + right
+        match parts[1] {
+            "+" => left + right,
+            "-" => left - right,
+            _ => panic!("Unsupported operation"),
+        }
     }
 }
 
@@ -21,5 +25,11 @@ mod tests {
     fn test_add() {
         let calculator = Calculator::new();
         assert_eq!(calculator.evaluate("2 + 3"), 5);
+    }
+
+    #[test]
+    fn test_subtract() {
+        let calculator = Calculator::new();
+        assert_eq!(calculator.evaluate("5 - 3"), 2);
     }
 }
