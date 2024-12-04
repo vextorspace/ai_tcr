1836525dbed4cf20fdae826f09a253c96590baea
Douglas Ronne <vextorspace@gmail.com>
2024-12-04 12:01:08 -0800

    Add calculator.rs with add function and tests.

---
+++ src/calc/calculator.rs
@@ -0,0 +1,15 @@
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+        assert_eq!(add(-1, 1), 0);
+        assert_eq!(add(0, 0), 0);
+    }
+}
--- src/calc/mod.rs
+++ src/calc/mod.rs
@@ -1,15 +1,2 @@
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
+pub mod calculator;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(add(2, 3), 5);
-        assert_eq!(add(-1, 1), 0);
-        assert_eq!(add(0, 0), 0);
-    }
-}
