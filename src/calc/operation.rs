64e16f73c60cc0750f7498b646bfcba4886adc68
Douglas Ronne <vextorspace@gmail.com>
2024-12-05 04:14:41 -0800

    Add operate method to Operation enum

--- src/calc/operation.rs
+++ src/calc/operation.rs
@@ -2,6 +2,14 @@ enum Operation {
     PLUS,
 }
 
+impl Operation {
+    fn operate(&self, a: i32, b: i32) -> i32 {
+        match self {
+            Operation::PLUS => a + b,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +17,12 @@ mod tests {
     #[test]
     fn instantiate() {
         let plus = Operation::PLUS;
+    }
+
+    #[test]
+    fn plus() {
+        let plus = Operation::PLUS;
+        let result = plus.operate(2, 3);
+        assert_eq!(result, 5);
     }
 }
