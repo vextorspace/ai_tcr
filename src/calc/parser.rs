9fc8a1766ea278af29087b82f06fa90c7b143f28
Douglas Ronne <vextorspace@gmail.com>
2024-12-05 07:29:41 -0800

    Remove redundant parse_numeric test function.

--- src/calc/parser.rs
+++ src/calc/parser.rs
@@ -31,12 +31,4 @@ mod tests {
         let result = parser.evaluate(Expression::new("1"));
         assert_eq!(result, Ok(1));
     }
-
-    #[test]
-    fn parse_numeric_gives_equivalent_numeric_expression() {
-        let parser = Parser::new();
-        let parsed: Vec<Expression> = parser.parse(Expression::new("1"));
-        assert_eq!(parsed.len(), 1);
-        assert_eq!(parsed[0], Expression::new("1"));
-    }
 }
