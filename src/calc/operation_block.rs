0c6385a1fe4d36f9f689d1e1f2d6e09bc3e28a46
Douglas Ronne <vextorspace@gmail.com>
2024-12-05 07:07:01 -0800

    Add Operation enum with PLUS variant

--- src/calc/operation_block.rs
+++ src/calc/operation_block.rs
@@ -1,1 +1,35 @@
+enum Operation {
+    PLUS,
+}
 
+impl Operation {
+    fn operate(&self, a: i32, b: i32) -> i32 {
+        match self {
+            Operation::PLUS => a + b,
+        }
+    }
+
+    fn symbol(&self) -> &str {
+        match self {
+            Operation::PLUS => "+",
+        }
+    }
+}
+
+struct OperationBlock;
+
+impl OperationBlock {
+    fn new() -> Self {
+        OperationBlock
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_implementation() {
+        let _ = OperationBlock::new();
+    }
+}
