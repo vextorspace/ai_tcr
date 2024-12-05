2895fb42c6b2a40e206273a210ec2f395e423f57
Douglas Ronne <vextorspace@gmail.com>
2024-12-05 04:29:51 -0800

    Add evaluate method to Expression struct

--- src/calc/expression.rs
+++ src/calc/expression.rs
@@ -4,6 +4,10 @@ impl Expression {
     fn new(_expr: &str) -> Self {
         Expression
     }
+
+    fn evaluate(&self) -> i32 {
+        2
+    }
 }
 
 #[cfg(test)]
@@ -13,5 +17,12 @@ mod tests {
     #[test]
     fn test_expression() {
         let expression = Expression::new("1 + 2 * 3");
+    }
+
+    #[test]
+    fn test_number_evaluates() {
+        let expression = Expression::new("2");
+        let value = expression.evaluate();
+        assert_eq!(value, 2);
     }
 }
