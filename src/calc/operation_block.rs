45a13d0f259455c36442bbdb265d9c5474899e96
Douglas Ronne <vextorspace@gmail.com>
2024-12-05 07:10:45 -0800

    Refactor: Add calc module with operations enum

--- src/calc/operation_block.rs
+++ src/calc/operation_block.rs
@@ -1,17 +1,51 @@
-pub struct OperationBlock;
+mod calc {
+    pub mod operation {
+        pub enum Operation {
+            PLUS,
+        }
 
-impl OperationBlock {
-    pub fn new() -> Self {
-        OperationBlock
+        impl Operation {
+            pub fn operate(&self, a: i32, b: i32) -> i32 {
+                match self {
+                    Operation::PLUS => a + b,
+                }
+            }
+
+            pub fn symbol(&self) -> &str {
+                match self {
+                    Operation::PLUS => "+",
+                }
+            }
+        }
+    }
+
+    pub mod operation_block {
+        use super::operation::Operation;
+
+        pub struct OperationBlock {
+            operation: Option<Operation>,
+        }
+
+        impl OperationBlock {
+            pub fn new() -> Self {
+                OperationBlock { operation: None }
+            }
+
+            pub fn with_operation(mut self, op: Operation) -> Self {
+                self.operation = Some(op);
+                self
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::calc::operation::Operation;
+    use super::calc::operation_block::OperationBlock;
 
     #[test]
     fn test_implementation() {
-        let _ = OperationBlock::new();
+        let _operation_block = OperationBlock::new().with_operation(Operation::PLUS);
     }
 }
