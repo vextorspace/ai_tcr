9d5e46676bcf8153fdf66a341734187f65791008
Douglas Ronne <vextorspace@gmail.com>
2024-12-05 04:40:52 -0800

    Add parse method to Parser struct

--- src/calc/parser.rs
+++ src/calc/parser.rs
@@ -4,6 +4,26 @@ impl Parser {
     fn new() -> Self {
         Parser
     }
+
+    fn parse(&self, expr: Expression) -> Result<i32, std::fmt::Error> {
+        expr.evaluate()
+    }
+}
+
+struct Expression {
+    expr: String,
+}
+
+impl Expression {
+    fn new(expr: &str) -> Self {
+        Expression {
+            expr: expr.to_string(),
+        }
+    }
+
+    fn evaluate(&self) -> Result<i32, std::fmt::Error> {
+        self.expr.parse::<i32>().map_err(|_| std::fmt::Error)
+    }
 }
 
 #[cfg(test)]
@@ -13,5 +33,12 @@ mod tests {
     #[test]
     fn instantiate() {
         let _ = Parser::new();
+    }
+
+    #[test]
+    fn parse_number_expression_gives_number() {
+        let parser = Parser::new();
+        let result = parser.parse(Expression::new("1"));
+        assert_eq!(result, Ok(1));
     }
 }
